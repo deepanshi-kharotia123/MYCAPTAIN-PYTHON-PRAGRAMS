e30019bb6bbd488c7de22c189a188d778bf9dc82
deepanshi-kharotia123 <87942924+deepanshi-kharotia123@users.noreply.github.com>
2021-07-31 12:12:52 +0530

    Add files via upload

---
+++ mycaptain task1(part1).py
@@ -0,0 +1,9 @@
+print('hello, word')
+
+ 
+from math import pi
+r = float(input ( " enter radius of circle : "))
+print ( " area of the circle is: "+ str(pi*r**2))
+
+
+
---
+++ mycaptain( positive numbers in a range).py
@@ -0,0 +1,11 @@
+# program to print positive numbers in a list
+
+#list of numbers
+list1 = [12, -7, 5, 64, -14]
+
+# interating each number in list1
+for num in list1 :
+    
+    # checking condition
+    if num >= 0:
+        print (num, end = " ")
